c7a7ef9fc8cd55c5e3f776c2e0ea783d8ff83f9b
Khlein-dev <kierjerymiepascua@gmail.com>
2025-09-26 23:00:04 +0800

    8th update

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const background = document.getElementsByTagName("body")[0];
 const search = document.getElementsByClassName("searchButton")[0];
 const github = document.getElementsByClassName("github")[0];
 const div = document.querySelectorAll("div")[0];
+const settings = document.getElementsByClassName("settings")[0];
+
+// Hover effects
 
 search.addEventListener("mouseover", () => {
     background.style.backgroundColor = "#54048aff";
@@ -25,4 +28,13 @@ github.addEventListener("mouseout", () => {
     background.style.transition = "800ms ease-in-out";
 });
 
+settings.addEventListener("mouseover", () => {
+    background.style.backgroundColor = "#ff7b00ff";
+    background.style.transition = "800ms ease-in-out";
+});
+
+settings.addEventListener("mouseout", () => {
+    background.style.backgroundColor = "#05053aff";
+    background.style.transition = "800ms ease-in-out";
+}); 
 
